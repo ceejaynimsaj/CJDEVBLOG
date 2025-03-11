9d976fe16f730f033fd27aaf0235a81e54c4007f
ceejaynimsaj <ceejaynimsaj@gmail.com>
2025-03-11 14:26:56 +0800

    CHANGES

--- script.js
+++ script.js
@@ -19,15 +19,12 @@ for (let i = 0; i < themeBtn.length; i++) {
 
   themeBtn[i].addEventListener('click', function () {
 
-    // toggle `light-theme` & `dark-theme` class from `body`
-    // when clicked `theme-btn`
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
     for (let i = 0; i < themeBtn.length; i++) {
 
-      // When the `theme-btn` is clicked,
-      // it toggles classes between `light` & `dark` for all `theme-btn`.
+      
       themeBtn[i].classList.toggle('light');
       themeBtn[i].classList.toggle('dark');
 
